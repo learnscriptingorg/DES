7058daa8c392a09e1b457cdf096956aaf26356a9
shaktids <shakti.ds@gmail.com>
2020-07-11 16:38:21 +0530

    Data Load

--- RCA/Demo/analysis/views.py
+++ RCA/Demo/analysis/views.py
@@ -11,7 +11,6 @@ def index(request):
          23, 42, 34, 23, 423, 4, 234, 234234, 23, 423, 4, 23, 42, 3423, 4, 23, 42, 34, 234, 23, 423, 4, 234, 23, 4, 23,
          423, 42, 34, 23, 423, 4, 234, 23, 4]
     mdict = {'a': 12, 'b': 14}
-
     br_name = request.GET.get('project_name')
 
     build_name = request.GET.get('build_name')
@@ -19,22 +18,19 @@ def index(request):
     print(request.GET.get('project_name'))
     br_names = [2, 3, 4, 43, 5, 345, 34, 5]
     build_nums = [3345, 45, 34, 543, 5, 345, 34, 5445, 35, 435, 4354, 5, 345534]
-    # db_ob=db_conn.db_conn()
-    # con=db_ob.get_conn()
-    # db_ob.close_conn(con)
-    # data_set=xml_data_parser.get_data()
-    # print(data_set)
-    data_set=db_conn.db_conn().get_data_table('Auroral_Sanity_Suite')
-
-    # if br_name == None:
-    #     build_nums = None
-    if br_name != None and build_name!=None:
-        return HttpResponse(json.dumps({'name': data_set}), content_type="application/json")
+    if request.method == "POST":
+        # Get the posted form
+        project_name = request.POST['project_name']
+        build_name=request.POST['build_name']
+        print('$$$$$$$',project_name,build_name)
+        data_set=db_conn.db_conn().get_data_table('Auroral_Sanity_Suite')
+        return render(request, 'load_build.html', {'data': data_set})
 
     if br_name != None:
-        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        #print("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
         return HttpResponse(json.dumps({'name': build_nums}), content_type="application/json")
     else:
+        #print("Inside main page ")
         return render(request, 'index1.html')
 
 
